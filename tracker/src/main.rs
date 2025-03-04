0b75210d987218e7d2bab1e48dd5f950124a03b7
Trust-Worthy <jondeveloper0@gmail.com>
2025-03-04 18:56:28 -0500

    Just doing some research on how to include errors. I want to thrown an error if the string once parsed, isn't actually an integer.

--- tracker/src/main.rs
+++ tracker/src/main.rs
@@ -1,3 +1,63 @@
+//! Finance Tracker in Rust!
+//! 
+//! Learning by doing is the best type of learning in my book!
+
+
+mod tracker_features {
+    pub mod expense_category;
+}
+use tracker_features::expense_category::ExpenseCategory;
+
+use core::num;
+use std::{io, num::ParseIntError};
+
+
+fn welcome_message() {
+
+    println!("\nWelcome to the finance tracker. You're going to be finacially smart!");
+    println!("Please choose something from the options below.");
+}
+
+
+fn get_user_selection() -> String {
+
+    println!("\nOptions are below");
+    println!("1. Add a new transaction");
+    println!("2. View transactions and summary within a date range");
+    println!("3. Exit");
+
+    let mut user_choice: String = String::new();
+
+
+    io::stdin()
+        .read_line(&mut user_choice)
+        .expect("Failed to read the input!");
+
+    return user_choice
+
+}
+
+fn check_user_input(user_choice: &String) -> Result<u32,ParseIntError> {
+
+    let num_choice: Result<u32, ParseIntError> = user_choice.trim().parse();
+
+    return num_choice
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    
+    welcome_message();
+
+    loop {
+
+        
+        let num_choice: Result<u32, ParseIntError> = check_user_input(&user_choice);
+
+        if num_choice.is_err() {
+
+        }
+
+
+    }
 }
---
+++ tracker/src/tracker_features/expense_category.rs
@@ -0,0 +1,11 @@
+// src/expense_category
+
+
+
+pub enum ExpenseCategory {
+    Food,
+    Transportation,
+    Entertainment,
+    Rent,
+    Other,
+}
