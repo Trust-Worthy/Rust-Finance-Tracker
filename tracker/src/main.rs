884089052aeeee7c8deb64b1705cacc96608d94c
Trust-Worthy <jondeveloper0@gmail.com>
2025-03-05 11:13:29 -0500

    First part works.

--- tracker/src/main.rs
+++ tracker/src/main.rs
@@ -3,15 +3,17 @@
 //! Learning by doing is the best type of learning in my book!
 
 mod user;
-use user::user_options::{check_user_input, get_user_selection};
+use user::user_options::{welcome_message,get_user_selection};
 
 mod tracker_features;
-use tracker_features
+
 
 
 
 
 fn main() {
+    welcome_message();
+    let user_selection: u32 = get_user_selection();
     
    
 }
