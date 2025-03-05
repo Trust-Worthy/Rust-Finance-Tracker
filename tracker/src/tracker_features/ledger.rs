eb6185bdf9aa09195075b72880e93982ae90db32
Trust-Worthy <jondeveloper0@gmail.com>
2025-03-05 14:41:41 -0500

    Almost done with the ledger. Added default values for start and end date.

--- tracker/src/tracker_features/ledger.rs
+++ tracker/src/tracker_features/ledger.rs
@@ -17,7 +17,7 @@ pub enum ExpenseCategory {
 
 impl fmt::Display for ExpenseCategory {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let category_str = match *self {
+        let category_str: &str = match *self {
             ExpenseCategory::Food => "Food",
             ExpenseCategory::Transportation => "Transportation",
             ExpenseCategory::Entertainment => "Entertainment",
@@ -31,10 +31,47 @@ impl fmt::Display for ExpenseCategory {
 }
 
 #[derive(Debug)]
+pub enum IncomeCategory {
+    WORK,
+    FREELANCE, 
+    GIFT,
+    OTHER,
+}
+
+impl fmt::Display for IncomeCategory {
+    fn fmt(&self, f:&mut fmt::Formatter<'_>) -> fmt::Result {
+        let category_str: &str = match *self {
+            IncomeCategory::WORK => "Work",
+            IncomeCategory::FREELANCE => "Freelance",
+            IncomeCategory::GIFT => "Gift",
+            IncomeCategory::OTHER => "Miscellaneous",
+        };
+
+        return write!(f,"{}",category_str)
+    }
+}
+
+#[derive(Debug)]
+pub enum TransactionType {
+    Income(IncomeCategory, f64),
+    Expense(ExpenseCategory, f64)
+
+}
+
+impl fmt::Display for TransactionType {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match *self {
+            TransactionType::Income(ref category,amount ) => write!(f,"Income: {} ${}",category,amount),
+            TransactionType::Expense(ref category, amount) => write!(f,"Expense: {} ${}",category,amount)
+        }
+    }
+}
+
+#[derive(Debug)]
 struct Transaction {
     date: NaiveDate,
     amount: f64,
-    category: ExpenseCategory,
+    _type: TransactionType,
     description: String,
 
 }
@@ -46,7 +83,7 @@ impl fmt::Display for Transaction {
         write!(
             f,
             "{:<10} {:>8.2}   {:<10}   {}", // aligns all the data
-            self.date, self.amount,self.category, self.description
+            self.date, self.amount,self._type, self.description
         )
     }
 }
@@ -75,12 +112,41 @@ impl Ledger {
         self.transactions.iter().map(|t:&Transaction|t.amount).sum()
     }
 
-    fn show_all_transactions(&self, start_date: NaiveDate, end_date: NaiveDate) {
+    fn show_all_transactions(&self, start_date: Option<NaiveDate>, end_date: Option<NaiveDate>) {
+        
+
+        let start_date: NaiveDate = start_date.unwrap_or_else(|| NaiveDate::from_ymd_opt(2000,1,1).expect("Valid default date"));
+        let end_date: NaiveDate = end_date.unwrap_or_else(|| NaiveDate::from_ymd_opt(2050,1,1).expect("Vaid default date"));
+
+
+        println!("Transactions from {} to {}", start_date, end_date);
+        println!("{:<15} {:<10} {:<15} {:<50}", "Date", "Amount", "Category", "Description");
+        println!("---------------------------------------------------------------");
+        for transaction in &self.transactions {
+            if transaction.date >= start_date && transaction.date <= end_date {
+                // Get the category as a string representation
+                let category = match &transaction._type {
+                    TransactionType::Income(ref category,_) => category.to_string(),
+                    TransactionType::Expense(ref category,_) => category.to_string(),
+                };
+                
+
+                let amount = match &transaction._type {
+                    TransactionType::Income(_,amount) => format!("{:2}",amount),
+                    TransactionType::Expense(_,amount) => format!("{:2}",amount),
+                };
+
+                println!("{:<15} {:<10} {:<15} {:<50}", 
+                transaction.date, 
+                amount, 
+                category, 
+                transaction.description);
+
+            }
+        }
+
 
     } 
 
-    fn get_transactions_by_category(&self, category: ExpenseCategory) -> {
-        
-    }
 
 }
